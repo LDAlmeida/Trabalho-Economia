490d68e2ddb69ef3977b970cf8569c596e5804cb
Lucas Almeida <ldalmeida.br@gmail.com>
2022-10-20 18:23:40 -0300

    final commit

--- tp1/economia/views.py
+++ tp1/economia/views.py
@@ -3,24 +3,22 @@ from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView
 from .models import Person
 from .forms import PersonForm
-class PersonCreateView(CreateView):
-    model = Person
-    fields = ('__all__')
-    form = PersonForm
-    
+
 def PersonView(request):
 
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        print("tst")
+        
         # create a form instance and populate it with data from the request:
         form = PersonForm(request.POST)
+        print(form.errors)
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            person = Person()
+            person = form.save(commit=True)
+            person.prever_emprego()
+            print(person.previsao)
+            return render(request,'economia/person_form.html', {'form': form, 'previsao': person.previsao})
 
     # if a GET (or any other method) we'll create a blank form
     else:
